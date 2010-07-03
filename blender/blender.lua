1ef0e9633f9bb9a2900a3a6f484b43ed112d4f2d
Andreas Kröhnke <ninzine@indraz.com>
2010-07-03 18:52:14 +0200

    Blender test updated to use texture, still a bit buggy.

--- blender/blender.lua
+++ blender/blender.lua
@@ -79,7 +79,7 @@ function p.build_mesh(mesh)
 	local m = {}
 	local vertices = {}
 	local triangles = {}
-	--local colors = blender_util.get_colors(mesh)
+	local uv_coords = {}
 
     print("blender> " .. mesh.totvert);
     for i=1, mesh.totvert do
@@ -93,7 +93,6 @@ function p.build_mesh(mesh)
 		table.insert(vertices, x)
 		table.insert(vertices, y)
 		table.insert(vertices, z)
-		print(string.byte(v.mat_nr))
     end
     
     print("blender> faces " .. mesh.totface)
@@ -101,47 +100,61 @@ function p.build_mesh(mesh)
     for i=1, mesh.totface do
         local f = mesh.mface[i]
         
-        local v1 = f.v1
-        local v2 = f.v2
-        local v3 = f.v3
-        local v4 = f.v4
-        
-        print("blender> indices (" .. v1 .. ", " .. v2 .. ", " .. v3
-			.. ", " .. v4 .. ")")
-		print(string.byte(f.mat_nr))
-
-		if v4 > 0 then
-			local tri = blender_util.quad_to_tri(f, mesh.mvert)
+        print("blender> indices (" .. f.v1 .. ", " .. f.v2 .. ", " .. f.v3
+			.. ", " .. f.v4 .. ")")
+		if f.v4 > 0 then
+			local tri = blender_util.quad_to_tri(f, mesh.mvert) 
 			for j=1, 6 do
 				table.insert(triangles, tri[j])
 			end
+
+			-- UV coords for mesh
+			if mesh.mtface then
+				uv_coords[f.v1 * 2 + 1] = mesh.mtface[i].uv[1]
+				uv_coords[f.v1 * 2 + 2] = mesh.mtface[i].uv[2]
+				uv_coords[f.v2 * 2 + 1] = mesh.mtface[i].uv[3]
+				uv_coords[f.v2 * 2 + 2] = mesh.mtface[i].uv[4]
+				uv_coords[f.v3 * 2 + 1] = mesh.mtface[i].uv[5]
+				uv_coords[f.v3 * 2 + 2] = mesh.mtface[i].uv[6]
+				uv_coords[f.v4 * 2 + 1] = mesh.mtface[i].uv[7]
+				uv_coords[f.v4 * 2 + 2] = mesh.mtface[i].uv[8]
+			end
 		else
-			table.insert(triangles, v1)
-			table.insert(triangles, v2)
-			table.insert(triangles, v3)
+			table.insert(triangles, f.v1)
+			table.insert(triangles, f.v2)
+			table.insert(triangles, f.v3)
+
+			-- UV coords for mesh
+			if mesh.mtface then
+				uv_coords[f.v1 * 2 + 1] = mesh.mtface[i].uv[1]
+				uv_coords[f.v1 * 2 + 2] = mesh.mtface[i].uv[2]
+				uv_coords[f.v2 * 2 + 1] = mesh.mtface[i].uv[3]
+				uv_coords[f.v2 * 2 + 2] = mesh.mtface[i].uv[4]
+				uv_coords[f.v3 * 2 + 1] = mesh.mtface[i].uv[5]
+				uv_coords[f.v3 * 2 + 2] = mesh.mtface[i].uv[6]
+			end
 		end
-        
-        if mesh.mtface then
-            -- UV coords are defined
-            local tf = mesh.mtface[i]
-            
-            --print("blender> uv " .. tf.uv[1][1])
-        end
     end
 
 	for i=1, table.maxn(triangles), 3 do
 		print("blender> triangle (" .. triangles[i] .. ", " .. triangles[i+1] ..
 		", " .. triangles[i+2] .. ")")
 	end
-
+	for i=1, table.maxn(uv_coords), 2 do
+		print("blender> uv (" .. uv_coords[i] .. ", " .. uv_coords[i+1] .. ")")
+	end
 
 	m.vertex = util.to_float(vertices)
 	m.triangle = util.to_uint16(triangles)
-	m.vertices = table.maxn(vertices)
+	m.vertices = table.maxn(vertices) / 3
 	m.triangles = table.maxn(triangles) / 3
-	--m.colors = table.maxn(colors) / 4
-	--m.color = util.to_float(colors)
-	
+	if mesh.mtface then
+		m.uv_coords = table.maxn(uv_coords)
+		m.uv = util.to_float(uv_coords)
+	end
+	print("faces: " .. mesh.totface .. " triangles: " .. m.triangles
+	.. " vertices: " .. m.vertices .. " uvs: " .. m.uv_coords / 2)
+
 	return m
 end
 
--- tests/blender_lua.lua
+++ tests/blender_lua.lua
@@ -1,21 +1,28 @@
-local b = blender.load("tests/drutten.blend")
+local b = blender.load("game/drutten.blend")
 local obj = blender.get_object(b, "Drutten.001")
 local mesh = blender.build_mesh(obj.data)
+local img = image.load("game/drutten.png")
 --local mesh = blender.build_scene(b.scenes[1])
 
 --blender.close(b)
 --[[m = model.open_cex("/Users/ninzine/Code/Lua/ConceptEngine/tests/export.cex")
 ]]--
 
---state = render.state()
+local state = render.state()
 render.create_window(320, 480)
 --render.setup_orthogonal_view(800, 600)
 render.setup_perspective_view(60, 320/480, 0.01, 90.0)
 render.set_viewport(0, 0, 320, 480)
+state.depth = render.generate_depthbuffer(320, 480)
 local i = -10
 local dir = 1
 local rotate = 0
 local rot = 0
+local tex = render.upload_texture(img.w, img.h, img.bpp, img.type, img.data)
+image.free(img)
+
+print("img> bpp: " .. img.bpp)
+render.enable_culling(2)
 function update()
     e = event.poll()
     while e.type ~= event.EMPTY do
@@ -45,15 +52,21 @@ function update()
     if rot > 359 then rot = 0 end
 
     --render.bind_buffers(state)
+    render.bind_depthbuffer(state)
     render.clear(0.0, 0.0, 0.0)
 	render.load_identity()
-	render.rotate(-90, 0, 0, 1)
+	--render.rotate(-90, 0, 0, 1)
 	--render.render_elements(m.vertex, m.vertices, m.face, m.faces)
 	render.color(1.0, 1.0, 1.0)
 	render.translate(0, 0, i)
 	render.rotate(-90, 1, 0, 0)
-	render.rotate(rot, 0, 1, 0)
+	render.rotate(rot, 0, 0, 1)
+    render.enable_texcoords(mesh.uv)
+    render.bind_texture(tex)
 	render.render_triangles(mesh.vertex, mesh.vertices, mesh.triangle, mesh.triangles)
+    --render.render_quad(10)
+    render.disable_texcoords()
+    render.bind_texture(0)
 	render.present()
     event.sleep(1)
 
