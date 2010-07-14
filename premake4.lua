3fd42afcfb9836961525238a5d22724abc36c773
Andreas Kröhnke <ninzine@indraz.com>
2010-07-15 00:17:43 +0200

    Add some dependencies to source tree. Integrate with premake script, update iPhone project. Premake now generates XCode projects too (not for iPhone). Ogg/Vorbis is not built on Linux at the moment. Zlib needs to be added.

--- premake4.lua
+++ premake4.lua
@@ -1,45 +1,61 @@
-solution "ConceptEngine"
+solution "FenrirEngine"
     configurations { "Debug", "Release" }
     platforms { "native" }
+	location "projects"
 
-    project "ConceptEngine"
-	kind "ConsoleApp"
-	language "C"
+	local INCLUDE_PATH = "/f/MinGW/include"
+	local LIB_PATH = "/f/MinGW/lib"
+	local SDL_PATH = "/f/dev/SDL-1.2.4"
+	local OPENAL_PATH = "/f/dev/OpenAL 1.1 SDK"
 	
-	configuration { "gmake" }
-		files { "source/*.c", "source/sdl/*.c", "source/win/*.c", "source/lua_wrap/*.c" }
+    project "FenrirEngine"
+		kind "ConsoleApp"
+		language "C"
 
+	configuration { "gmake" }
 		if true == os.is("windows") then
 		    buildoptions { "-Wall" }
 		    defines { "__SDL__", "WIN32" }
 		    files { "source/*.c", "source/sdl/*.c", "source/win/*.c",
 			    "source/lua_wrap/*.c" }
-		    includedirs {"include", "/f/dev/SDL-1.2.14/include", "/usr/local/include",
-			    "/f/dev/lua-5.1.4/src",
-			    "/f/MinGW/include",
-			    "/f/dev/OpenAL 1.1 SDK/include",
-			    }
-		    libdirs { "/f/dev/SDL-1.2.14/lib", "/usr/local/lib", "/f/dev/lua-5.1.4/src",
-			    "/f/MinGW/lib", "/f/dev/OpenAL 1.1 SDK/libs/Win32"
-			    }
-		    links { "mingw32", "SDLmain", "SDL", "lua", "ogg", "vorbis", 
-		    "vorbisfile", "opengl32", "openal32", "png", "ws2_32",
-		    "m", }
+		    includedirs {"include", "lib", "lib/lua", "lib/png",
+				SDL_PATH .. "/include", "/usr/local/include",
+				INCLUDE_PATH, OPENAL_PATH .. "/include" }
+		    libdirs { SDL_PATH .. "/lib", "/usr/local/lib", LIB_PATH,
+				OPENAL_PATH .. "/libs/Win32" }
+		    links { "mingw32", "SDLmain", "SDL", "opengl32", "openal32", "ws2_32",
+				"m", "z", "lua", "png", "ogg_vorbis" }
 		    linkoptions { "-Wl,--subsystem,console", "-Wl,-u,_WinMain@16" }
-		else
+
+		elseif true == os.is("linux") then
 			buildoptions {"-Wall"}
 			defines {"__SDL__"}
-			includedirs {"include", "/usr/include", "/usr/include/lua5.1"}
-			libdirs {"/usr/lib"}
-			links {"SDLmain", "SDL", "lua5.1", "GL", "png", "ogg", "vorbis",
-                "vorbisfile", "openal", "m"}
+		    files { "source/*.c", "source/sdl/*.c", "source/win/*.c",
+			    "source/lua_wrap/*.c" }
+			includedirs {"include", "lib", "lib/lua", "lib/png", INCLUDE_PATH }
+			libdirs { LIB_PATH }
+			links {"SDLmain", "SDL", "GL", "openal", "m", "z", "lua", "png",
+				"ogg", "vorbis", "vorbisfile" }
 		end
 
 	configuration { "gmake", "Debug" }
 	    defines { "DEBUG" }
 	    buildoptions { "-gdwarf-2" }
 
-	configuration { "vs2008" }
+	configuration { "xcode3" }
+		files { "include/**.h", "source/*.c", "source/sdl/*.c", "source/osx/*.m",
+		"source/lua_wrap/*.c" }
+
+		buildoptions {"-Wall"}
+		defines {"__SDL__"}
+		includedirs {"include", "lib", "lib/lua", "lib/png", INCLUDE_PATH }
+		libdirs { LIB_PATH }
+		links {"lua", "Cocoa.framework", "SDL.framework", "OpenGL.framework",
+			"OpenAL.framework", "m", "z", "png", "ogg_vorbis" }
+	
+	--[[ TODO: Needs to be updated (by someone that uses it). Compiler still needs
+	to be MinGW though ]]--
+	configuration { "vs2008", "vs2005" }
 	    defines { "__SDL__", "WIN32", "_USE_MATH_DEFINES" }
 	    files { "source/*.c", "source/sdl/*.c", "source/win/*.c",
 		    "source/lua_wrap/*.c" }
@@ -48,8 +64,29 @@ solution "ConceptEngine"
 		    "D:/dev/OpenAL 1.1 SDK/include" }
 	    libdirs { "D:/dev/SDL-1.2.14/lib", "D:/dev/lua-5.1.4-30/lib",
 		   "D:/dev/OpenAL 1.1 SDK/libs/Win32" }
-	    links { "SDLmain", "opengl32", "openal32", "SDL",
-	    "lua51" }
+	    links { "SDLmain", "opengl32", "openal32", "SDL" }
 	    linkoptions {  }
+
+
+	-- Dependencies
+	project "lua"
+		kind "StaticLib"
+		language "C"
+		files { "lib/lua/*.c" }
+		includedirs { "include" }
+
+	project "png"
+		kind "StaticLib"
+		language "C"
+		includedirs { INCLUDE_PATH }
+		files { "lib/png/*.c" }
+
+	if false == os.is("linux") then
+	project "ogg_vorbis"
+		kind "StaticLib"
+		language "C"
+		files { "lib/ogg/*.c", "lib/vorbis/**.c" }
+		includedirs { "lib/ogg", "lib/vorbis" }
+	end
 
 
